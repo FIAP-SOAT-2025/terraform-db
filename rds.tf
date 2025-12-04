e212db8a12c4b09192592db4e7ff4c7ecc69ee59
Felipe <alves.felipeteixeira@gmail.com>
2025-12-04 01:26:16 -0300

    update tc version

--- rds.tf
+++ rds.tf
@@ -40,7 +40,7 @@ resource "aws_db_instance" "db_instance" {
   engine_version         = "15"
   instance_class         = "db.t3.micro"
   allocated_storage      = 20
-  db_name                = var.db_name
+  # db_name                = var.db_name
   username               = var.db_user
   password               = var.db_password
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
